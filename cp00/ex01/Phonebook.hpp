7c145b62c470bc955eb0e51de0d6fcc99eea4fa9
Roger Sebastien <rsebasti@c2r3p5.42perpignan.fr>
2025-03-06 14:16:23 +0100

    better I guess

--- cp00/ex01/Phonebook.hpp
+++ cp00/ex01/Phonebook.hpp
@@ -6,7 +6,7 @@
 /*   By: rsebasti <rsebasti@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/19 19:48:01 by rsebasti          #+#    #+#             */
-/*   Updated: 2025/02/20 18:06:17 by rsebasti         ###   ########.fr       */
+/*   Updated: 2025/03/06 13:24:48 by rsebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 #define PHONEBOOK_HPP
 
 #include "main.hpp"
-#include "iostream"
-#include "iomanip"
+#include <iostream>
+#include <iomanip>
 
 class Phonebook
 {
--- cp00/ex02/Account.cpp
+++ cp00/ex02/Account.cpp
@@ -6,11 +6,39 @@
 /*   By: rsebasti <rsebasti@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 15:00:17 by rsebasti          #+#    #+#             */
-/*   Updated: 2025/02/27 15:15:43 by rsebasti         ###   ########.fr       */
+/*   Updated: 2025/03/06 14:13:57 by rsebasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <iterator>
+#include <locale>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit)
+{
+	_amount = initial_deposit;
+	_accountIndex = _nbAccounts;
+	_nbAccounts++;
+	_totalAmount += initial_deposit;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";created" << std::endl;
+}
+
+Account::~Account() 
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";closed" << std::endl;
+}
 
 int	Account::getNbAccounts()
 {
@@ -32,3 +60,59 @@ int	Account::getNbWithdrawals()
 	return (_totalNbWithdrawals);
 }
 
+void	Account::displayAccountsInfos()
+{
+	_displayTimestamp();
+	std::cout << "account:" << _nbAccounts << ";total:" << _totalAmount << ";deposits:" << _totalNbDeposits <<";withdrawals:" << _totalNbWithdrawals << std::endl;
+}
+
+void	Account::makeDeposit( int deposit )
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";p_amount:" << _amount;
+	_totalNbDeposits++;
+	_nbDeposits++;
+	_totalAmount += deposit;
+	_amount += deposit;
+	std::cout << ";deposit:" << deposit << ";amount:" << _amount << ";nb_deposits:" << _nbDeposits << std::endl;
+}
+bool	Account::makeWithdrawal( int withdrawal )
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";p_amount:" << _amount;
+	if (withdrawal > _amount)
+	{
+		std::cout  << ";withdrawal:refused" << std::endl;
+		return (0);
+	}
+	_amount -= withdrawal;
+	_totalAmount -= withdrawal;
+	_nbWithdrawals++;
+	_totalNbWithdrawals++;
+	std::cout  << ";withdrawal:" << withdrawal << ";amount:" << _amount << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
+	return (1);
+}
+int		Account::checkAmount( void ) const
+{
+	return (_amount);
+}
+void	Account::displayStatus( void ) const
+{
+	_displayTimestamp();
+	std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";deposits:" << _nbDeposits << ";withdrawals:" <<_nbWithdrawals << std::endl;
+}
+
+void	Account::_displayTimestamp(void)
+{
+    time_t rawtime;
+    struct tm * timeinfo;
+	
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    std::cout << "[" << (1900 + timeinfo->tm_year)
+              << std::setw(2) << std::setfill('0') << (timeinfo->tm_mon + 1)
+              << std::setw(2) << std::setfill('0') << timeinfo->tm_mday << "_"
+              << std::setw(2) << std::setfill('0') << timeinfo->tm_hour
+              << std::setw(2) << std::setfill('0') << timeinfo->tm_min
+              << std::setw(2) << std::setfill('0') << timeinfo->tm_sec << "] ";
+}
