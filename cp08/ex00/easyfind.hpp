3510a9557515986126e7d275a6ccece6e48bad5f
Roger Sebastien <rsebasti@c2r3p5.42perpignan.fr>
2025-04-04 12:22:03 +0200

    fix bug

---
+++ cp08/ex00/easyfind.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+
+template <typename T>
+T easyfind(std::vector<T> container, T toSearch)
+{
+	for (std::vector<T>::iterator it = container.begin(); it != container.end(); it++)
+	{
+		if (&it == T)
+			return T;
+	}
+}
